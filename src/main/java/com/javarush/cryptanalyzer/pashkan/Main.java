a141bce9dedf596b7222db94ceb1626ed10a8fc3
Aleksei <alekseipashkan@gmail.com>
2023-05-02 18:26:56 -0400

    First commit1

--- src/main/java/com/javarush/cryptanalyzer/pashkan/Main.java
+++ src/main/java/com/javarush/cryptanalyzer/pashkan/Main.java
@@ -2,6 +2,6 @@ package com.javarush.cryptanalyzer.pashkan;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hwllo world!");
+        System.out.println("Hello world!");
     }
 }
